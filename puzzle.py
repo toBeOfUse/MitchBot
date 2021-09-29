0f9fd5c8a90607c6d118066385055615e9578a79
toBeOfUse <mitchjacov@gmail.com>
2021-09-29 13:38:15 -0400

    refactor Puzzle class-specific code to puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,166 @@
+import json
+import math
+from io import BytesIO
+import re
+
+from tornado.httpclient import AsyncHTTPClient
+from PIL import Image, ImageDraw, ImageFont
+import discord
+
+
+class Puzzle():
+    # constants returned by `guess(word)`:
+    wrong_word = 1
+    good_word = 2
+    pangram = 3
+
+    def __init__(self, center: str, outside: list[str], pangrams: list[str], answers: list[str]):
+        self.center = center.upper()
+        self.outside = [l.upper() for l in outside]
+        self.pangrams = set(p.lower() for p in pangrams)
+        self.answers = set(a.lower() for a in answers)
+        self.gotten_words = set()
+
+    def __eq__(self, other):
+        return self.center+self.outside == other.center+other.outside
+
+    @property
+    def percentageComplete(self):
+        return round(len(self.gotten_words) / len(self.answers) * 100, 1)
+
+    def does_word_count(self, word: str) -> bool:
+        return word.lower() in self.answers
+
+    def is_pangram(self, word: str) -> bool:
+        return word.lower() in self.pangrams
+
+    def guess(self, word: str) -> int:
+        """
+        determines whether a word counts for a point (i. e. is in `self.answers` and
+        hasn't been tried before) or is a pangram (that hasn't been guessed before.)
+        uses arbitrary constants defined on the class.
+        """
+        w = word.lower()
+        if self.is_pangram(w):
+            self.gotten_words.add(w)
+            return self.pangram
+        elif self.does_word_count(w):
+            self.gotten_words.add(w)
+            return self.good_word
+        else:
+            return self.wrong_word
+
+    @staticmethod
+    def make_hexagon(width: int, height: int, tilted: bool = False) -> list[tuple[int, int]]:
+        # starting with the leftmost point, construct a hexagon with the given
+        # dimensions centered on 0,0 so it can be trivially translated to center on
+        # whatever we want and also scaled. for a regular hexagon, width should be
+        # sqrt(3) times the height or vice versa. points go clockwise. if tilted is
+        # True, the sides are parallel to the edges of the image instead of the top
+        # and bottom, and the topmost point comes first
+        base = [(-width / 2, 0),
+                (-width/4, -height/2),
+                (width/4, -height/2),
+                (width/2, 0),
+                (width/4, height/2),
+                (-width/4, height/2)]
+        return [(tuple(reversed(x)) if tilted else x) for x in base]
+
+    def render(self, output_width: int = 600) -> BytesIO:
+        base_hex_side = 10
+        base_width = base_hex_side*2
+        base_height = math.sqrt(3) * base_hex_side
+        # used for base hexagon
+        hexpoints = self.make_hexagon(base_width, base_height)
+        # used for placement of the centers of the base hexagon
+        rotated_hexpoints = self.make_hexagon(base_width, base_height, True)
+        # to perfectly tile the hexagons, we would place their centers at the points
+        # of a hexagon sqrt(3) times larger than the base one. we are scaling them up a
+        # little more here for spacing.
+        hexcenters = [(x*(math.sqrt(3)+0.2), y*(math.sqrt(3)+0.2)) for x, y in rotated_hexpoints]
+        # this is the width (longest side) of the image, which we will use to make a
+        # square resulting image. it needs to be at least. something.
+        base_image_width = base_width*3.25
+        anti_alias_level = 4
+        temp_width = output_width * anti_alias_level
+        actual_to_base = temp_width/base_image_width
+        image = Image.new("RGBA", (temp_width, temp_width), 0)
+        surface = ImageDraw.Draw(image)
+        # experimental background
+        for i in range(8):
+            surface.regular_polygon(
+                bounding_circle=(temp_width/2, temp_width/2, temp_width/1.8),
+                n_sides=6,
+                fill=(255, 255, 255, int(100 + 155/(8-i))),
+                rotation=360/8*i)
+        # bg dodecahedron, scaled to the canvas size and then translated to be centered
+        surface.regular_polygon(
+            bounding_circle=(temp_width/2, temp_width/2, temp_width/2),
+            n_sides=12,
+            fill=(255, 255, 255, 255))
+        font = ImageFont.truetype("./fonts/LiberationSans-Bold.ttf",
+                                  round(base_height*0.4*actual_to_base))
+        # center points of the base hexagons, scaled to the canvas size and then
+        # translated to be centered
+        hexcenters = [(x*actual_to_base+temp_width/2, y*actual_to_base+temp_width/2)
+                      for x, y in hexcenters]
+        letter_iterator = iter(self.outside)
+        for hexcenter in hexcenters:
+            surface.polygon(
+                [(x * actual_to_base + hexcenter[0],
+                  y * actual_to_base + hexcenter[1])
+                 for x, y in hexpoints],
+                fill="#E6E6E6FF")
+            surface.text((hexcenter[0], hexcenter[1]), next(
+                letter_iterator), fill="#000000ff", font=font, anchor="mm")
+        # draw center base polygon
+        surface.polygon(
+            [(x * actual_to_base + temp_width/2,
+              y * actual_to_base + temp_width/2)
+             for x, y in hexpoints],
+            fill="#F7DA21FF")
+        surface.text((temp_width/2, temp_width/2), self.center,
+                     fill="#000000ff", font=font, anchor="mm")
+
+        image = image.resize((output_width, output_width), resample=Image.LANCZOS)
+        image_bytes = BytesIO()
+        image.save(image_bytes, format='PNG')
+        image_bytes.seek(0)
+        return image_bytes
+
+    @classmethod
+    async def fetch_from_nyt(cls) -> "Puzzle":
+        client = AsyncHTTPClient()
+        response = await client.fetch("https://www.nytimes.com/puzzles/spelling-bee")
+        html = response.body.decode("utf-8")
+        game_data = re.search("window.gameData = (.*?)</script>", html)
+        if game_data:
+            game = json.loads(game_data.group(1))["today"]
+            return cls(
+                game["centerLetter"],
+                game["outerLetters"],
+                game["pangrams"],
+                game["answers"])
+
+    async def respond_to_guesses(self, message: discord.Message):
+        """
+        Discord bot-specific function for awarding points in the form of reactions
+        """
+        num_emojis = ["0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]
+        words = re.sub("\W", " ", message.content).split()
+        points = 0
+        pangram = False
+        for word in words:
+            guess_result = self.guess(word)
+            if guess_result == Puzzle.good_word:
+                points += 1
+            if guess_result == Puzzle.pangram:
+                points += 1
+                pangram = True
+        if points > 0:
+            await message.add_reaction("👍")
+            if points > 1:
+                for num_char in str(points):
+                    await message.add_reaction(num_emojis[int(num_char)])
+        if pangram:
+            await message.add_reaction("🍳")
--- scheduler.py
+++ scheduler.py
@@ -1,154 +1,15 @@
 import asyncio
-from io import BytesIO
-import math
 import re
-import json
 from datetime import time, datetime, timezone
 import inspect
 import random
 from typing import Callable
 
 import discord
-from tornado.httpclient import AsyncHTTPClient
-from PIL import Image, ImageDraw, ImageFont
 
 from MitchBot import MessageResponder, MitchClient
 from textresources import poetry_generator
-
-
-class Puzzle():
-    # constants returned by `guess(word)`:
-    wrong_word = 1
-    good_word = 2
-    pangram = 3
-
-    def __init__(self, center: str, outside: list[str], pangrams: list[str], answers: list[str]):
-        self.center = center.upper()
-        self.outside = [l.upper() for l in outside]
-        self.pangrams = set(p.lower() for p in pangrams)
-        self.answers = set(a.lower() for a in answers)
-        self.gotten_words = set()
-
-    def __eq__(self, other):
-        return self.center+self.outside == other.center+other.outside
-
-    @property
-    def percentageComplete(self):
-        return round(len(self.gotten_words) / len(self.answers) * 100, 1)
-
-    def does_word_count(self, word: str) -> bool:
-        return word.lower() in self.answers
-
-    def is_pangram(self, word: str) -> bool:
-        return word.lower() in self.pangrams
-
-    def guess(self, word: str) -> int:
-        """
-        determines whether a word counts for a point (i. e. is in `self.answers` and
-        hasn't been tried before) or is a pangram (that hasn't been guessed before.)
-        uses arbitrary constants defined on the class.
-        """
-        w = word.lower()
-        if self.is_pangram(w):
-            self.gotten_words.add(w)
-            return self.pangram
-        elif self.does_word_count(w):
-            self.gotten_words.add(w)
-            return self.good_word
-        else:
-            return self.wrong_word
-
-    @staticmethod
-    def make_hexagon(width: int, height: int, tilted: bool = False) -> list[tuple[int, int]]:
-        # starting with the leftmost point, construct a hexagon with the given
-        # dimensions centered on 0,0 so it can be trivially translated to center on
-        # whatever we want and also scaled. for a regular hexagon, width should be
-        # sqrt(3) times the height or vice versa. points go clockwise. if tilted is
-        # True, the sides are parallel to the edges of the image instead of the top
-        # and bottom, and the topmost point comes first
-        base = [(-width / 2, 0),
-                (-width/4, -height/2),
-                (width/4, -height/2),
-                (width/2, 0),
-                (width/4, height/2),
-                (-width/4, height/2)]
-        return [(tuple(reversed(x)) if tilted else x) for x in base]
-
-    def render(self, output_width: int = 600) -> BytesIO:
-        base_hex_side = 10
-        base_width = base_hex_side*2
-        base_height = math.sqrt(3) * base_hex_side
-        # used for base hexagon
-        hexpoints = self.make_hexagon(base_width, base_height)
-        # used for placement of the centers of the base hexagon
-        rotated_hexpoints = self.make_hexagon(base_width, base_height, True)
-        # to perfectly tile the hexagons, we would place their centers at the points
-        # of a hexagon sqrt(3) times larger than the base one. we are scaling them up a
-        # little more here for spacing.
-        hexcenters = [(x*(math.sqrt(3)+0.2), y*(math.sqrt(3)+0.2)) for x, y in rotated_hexpoints]
-        # this is the width (longest side) of the image, which we will use to make a
-        # square resulting image. it needs to be at least. something.
-        base_image_width = base_width*3.25
-        anti_alias_level = 4
-        temp_width = output_width * anti_alias_level
-        actual_to_base = temp_width/base_image_width
-        image = Image.new("RGBA", (temp_width, temp_width), 0)
-        surface = ImageDraw.Draw(image)
-        # experimental background
-        for i in range(8):
-            surface.regular_polygon(
-                bounding_circle=(temp_width/2, temp_width/2, temp_width/1.8),
-                n_sides=6,
-                fill=(255, 255, 255, int(100 + 155/(8-i))),
-                rotation=360/8*i)
-        # bg dodecahedron, scaled to the canvas size and then translated to be centered
-        surface.regular_polygon(
-            bounding_circle=(temp_width/2, temp_width/2, temp_width/2),
-            n_sides=12,
-            fill=(255, 255, 255, 255))
-        font = ImageFont.truetype("./fonts/LiberationSans-Bold.ttf",
-                                  round(base_height*0.4*actual_to_base))
-        # center points of the base hexagons, scaled to the canvas size and then
-        # translated to be centered
-        hexcenters = [(x*actual_to_base+temp_width/2, y*actual_to_base+temp_width/2)
-                      for x, y in hexcenters]
-        letter_iterator = iter(self.outside)
-        for hexcenter in hexcenters:
-            surface.polygon(
-                [(x * actual_to_base + hexcenter[0],
-                  y * actual_to_base + hexcenter[1])
-                 for x, y in hexpoints],
-                fill="#E6E6E6FF")
-            surface.text((hexcenter[0], hexcenter[1]), next(
-                letter_iterator), fill="#000000ff", font=font, anchor="mm")
-        # draw center base polygon
-        surface.polygon(
-            [(x * actual_to_base + temp_width/2,
-              y * actual_to_base + temp_width/2)
-             for x, y in hexpoints],
-            fill="#F7DA21FF")
-        surface.text((temp_width/2, temp_width/2), self.center,
-                     fill="#000000ff", font=font, anchor="mm")
-
-        image = image.resize((output_width, output_width), resample=Image.LANCZOS)
-        image_bytes = BytesIO()
-        image.save(image_bytes, format='PNG')
-        image_bytes.seek(0)
-        return image_bytes
-
-    @classmethod
-    async def fetch_from_nyt(cls) -> "Puzzle":
-        client = AsyncHTTPClient()
-        response = await client.fetch("https://www.nytimes.com/puzzles/spelling-bee")
-        html = response.body.decode("utf-8")
-        game_data = re.search("window.gameData = (.*?)</script>", html)
-        if game_data:
-            game = json.loads(game_data.group(1))["today"]
-            return cls(
-                game["centerLetter"],
-                game["outerLetters"],
-                game["pangrams"],
-                game["answers"])
+from puzzle import Puzzle
 
 
 async def do_thing_after(seconds: float, thing: Callable):
@@ -213,29 +74,11 @@ def schedule_tasks(client: MitchClient):
     asyncio.create_task(repeatedly_schedule_task_for(fetch_new_puzzle_at, send_new_puzzle))
 
     async def respond_to_guesses(message: discord.Message):
-        if current_puzzle is None:
-            return
-        num_emojis = ["0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]
-        words = re.sub("\W", " ", message.content).split()
-        points = 0
-        pangram = False
-        for word in words:
-            guess_result = current_puzzle.guess(word)
-            if guess_result == Puzzle.good_word:
-                points += 1
-            if guess_result == Puzzle.pangram:
-                points += 1
-                pangram = True
-        if points > 0:
-            await message.add_reaction("👍")
-            if points > 1:
-                for num_char in str(points):
-                    await message.add_reaction(num_emojis[int(num_char)])
-        if pangram:
-            await message.add_reaction("🍳")
-        if current_puzzle.percentageComplete > 0 and last_puzzle_post is not None:
-            base_content = re.sub("\(.*\)", "", last_puzzle_post.content).strip()
-            await last_puzzle_post.edit(content=base_content + f" ({current_puzzle.percentageComplete}% complete)")
+        if current_puzzle is not None:
+            await current_puzzle.respond_to_guesses(message)
+            if current_puzzle.percentageComplete > 0 and last_puzzle_post is not None:
+                base_content = re.sub("\(.*\)", "", last_puzzle_post.content).strip()
+                await last_puzzle_post.edit(content=base_content + f" ({current_puzzle.percentageComplete}% complete)")
 
     client.register_responder(MessageResponder(
         lambda m: m.channel.id == puzzle_channel_id, respond_to_guesses))
