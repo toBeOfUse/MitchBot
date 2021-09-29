89d3aa688ef99c3dba9115a1d643959335c2b016
toBeOfUse <mitchjacov@gmail.com>
2021-09-29 17:41:20 -0400

    use svg resources for puzzle background

--- puzzle.py
+++ puzzle.py
@@ -1,12 +1,12 @@
 import asyncio
-import json
-import math
 from io import BytesIO
+import json
 import re
 import sqlite3
+from PIL import Image
 
 from tornado.httpclient import AsyncHTTPClient
-from PIL import Image, ImageDraw, ImageFont
+from cairosvg import svg2png
 import discord
 
 words_db = sqlite3.connect("words.db")
@@ -71,83 +71,13 @@ class Puzzle():
         unguessed.sort(key=lambda w: get_word_frequency(w))
         return unguessed
 
-    @staticmethod
-    def make_hexagon(width: int, height: int, tilted: bool = False) -> list[tuple[int, int]]:
-        # starting with the leftmost point, construct a hexagon with the given
-        # dimensions centered on 0,0 so it can be trivially translated to center on
-        # whatever we want and also scaled. for a regular hexagon, width should be
-        # sqrt(3) times the height or vice versa. points go clockwise. if tilted is
-        # True, the sides are parallel to the edges of the image instead of the top
-        # and bottom, and the topmost point comes first
-        base = [(-width / 2, 0),
-                (-width/4, -height/2),
-                (width/4, -height/2),
-                (width/2, 0),
-                (width/4, height/2),
-                (-width/4, height/2)]
-        return [(tuple(reversed(x)) if tilted else x) for x in base]
-
-    def render(self, output_width: int = 600) -> BytesIO:
-        base_hex_side = 10
-        base_width = base_hex_side*2
-        base_height = math.sqrt(3) * base_hex_side
-        # used for base hexagon
-        hexpoints = self.make_hexagon(base_width, base_height)
-        # used for placement of the centers of the base hexagon
-        rotated_hexpoints = self.make_hexagon(base_width, base_height, True)
-        # to perfectly tile the hexagons, we would place their centers at the points
-        # of a hexagon sqrt(3) times larger than the base one. we are scaling them up a
-        # little more here for spacing.
-        hexcenters = [(x*(math.sqrt(3)+0.2), y*(math.sqrt(3)+0.2)) for x, y in rotated_hexpoints]
-        # this is the width (longest side) of the image, which we will use to make a
-        # square resulting image. it needs to be at least. something.
-        base_image_width = base_width*3.25
-        anti_alias_level = 4
-        temp_width = output_width * anti_alias_level
-        actual_to_base = temp_width/base_image_width
-        image = Image.new("RGBA", (temp_width, temp_width), 0)
-        surface = ImageDraw.Draw(image)
-        # experimental background
-        for i in range(8):
-            surface.regular_polygon(
-                bounding_circle=(temp_width/2, temp_width/2, temp_width/1.8),
-                n_sides=6,
-                fill=(255, 255, 255, int(100 + 155/(8-i))),
-                rotation=360/8*i)
-        # bg dodecahedron, scaled to the canvas size and then translated to be centered
-        surface.regular_polygon(
-            bounding_circle=(temp_width/2, temp_width/2, temp_width/2),
-            n_sides=12,
-            fill=(255, 255, 255, 255))
-        font = ImageFont.truetype("./fonts/LiberationSans-Bold.ttf",
-                                  round(base_height*0.4*actual_to_base))
-        # center points of the base hexagons, scaled to the canvas size and then
-        # translated to be centered
-        hexcenters = [(x*actual_to_base+temp_width/2, y*actual_to_base+temp_width/2)
-                      for x, y in hexcenters]
-        letter_iterator = iter(self.outside)
-        for hexcenter in hexcenters:
-            surface.polygon(
-                [(x * actual_to_base + hexcenter[0],
-                  y * actual_to_base + hexcenter[1])
-                 for x, y in hexpoints],
-                fill="#E6E6E6FF")
-            surface.text((hexcenter[0], hexcenter[1]), next(
-                letter_iterator), fill="#000000ff", font=font, anchor="mm")
-        # draw center base polygon
-        surface.polygon(
-            [(x * actual_to_base + temp_width/2,
-              y * actual_to_base + temp_width/2)
-             for x, y in hexpoints],
-            fill="#F7DA21FF")
-        surface.text((temp_width/2, temp_width/2), self.center,
-                     fill="#000000ff", font=font, anchor="mm")
-
-        image = image.resize((output_width, output_width), resample=Image.LANCZOS)
-        image_bytes = BytesIO()
-        image.save(image_bytes, format='PNG')
-        image_bytes.seek(0)
-        return image_bytes
+    def render(self, output_width: int = 600) -> bytes:
+        with open("images/puzzle_bg_1.svg") as base_file:
+            base_svg = base_file.read()
+        base_svg = base_svg.replace("%center%", self.center)
+        for letter in self.outside:
+            base_svg = base_svg.replace("%letter%", letter, 1)
+        return svg2png(base_svg, output_width=output_width)
 
     @classmethod
     async def fetch_from_nyt(cls) -> "Puzzle":
@@ -189,8 +119,11 @@ class Puzzle():
 
 async def test():
     print("frequency of 'puzzle'", get_word_frequency("puzzle"))
+    puzzle = await Puzzle.fetch_from_nyt()
     print("today's words from least to most common:")
-    print((await Puzzle.fetch_from_nyt()).get_unguessed_words())
+    print(puzzle.get_unguessed_words())
+    rendered = puzzle.render()
+    Image.open(BytesIO(rendered)).show()
 
 
 if __name__ == "__main__":
