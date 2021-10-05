a455aabf6da50b8025d2f0cb2ad178eff4930b71
toBeOfUse <mitchjacov@gmail.com>
2021-10-05 03:54:48 -0400

    fix gcc warning

--- db/trieparse.pyx
+++ db/trieparse.pyx
@@ -6,5 +6,5 @@ cdef extern from "./src/trieparse.h":
     const char* search_words(const char* trie_buffer, const char* eligible_characters)
 
 cpdef search(bytes trie_buffer, bytes eligible_characters):
-    cdef char * result = search_words(<bytes>trie_buffer, eligible_characters)
+    cdef const char * result = search_words(<bytes>trie_buffer, eligible_characters)
     return <bytes> result
