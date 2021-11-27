0d071f52b088d98f4504c84df2aa775ece830ca8
toBeOfUse <mitchjacov@gmail.com>
2021-11-27 03:12:23 -0500

    finally start logging

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # python libraries
 import asyncio
+import logging
 
 # project files
 import MitchBot
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(name)s: %(message)s'))
+logger.addHandler(handler)
 
 
 async def main():
